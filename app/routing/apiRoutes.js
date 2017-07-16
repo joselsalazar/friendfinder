1d2fbcdb689e15dea5c02426db601b0b118ef685
Jose Salazar <jose.salazar.home@gmail.com>
2017-07-15 20:07:46 -0700

    Presentable Changes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  app.post("/api/tables", function(req, res) {
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    tableData = [];
+    waitListData = [];
+
+    console.log(tableData);
+  });
+};
